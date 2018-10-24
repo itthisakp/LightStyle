ffdb8d2c328fba355f41474cf05811e8ceb51c63
Toei <itthisakp@hotmail.com>
2018-10-24 12:23:51 +0700

    update framework

---
+++ LightStyleSDK.framework/Headers/LightStyleSDK.h
@@ -0,0 +1,18 @@
+//
+//  LightStyleSDK.h
+//  LightStyleSDK
+//
+//  Created by Jeremy on 3/7/2561 BE.
+//  Copyright © 2561 Jeremy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LightStyleSDK.
+FOUNDATION_EXPORT double LightStyleSDKVersionNumber;
+
+//! Project version string for LightStyleSDK.
+FOUNDATION_EXPORT const unsigned char LightStyleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightStyleSDK/PublicHeader.h>
+
